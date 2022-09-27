345e7d8a503b7c62e84620c7656022d5458975c5
swiftsuretech <dave@davewhitehouse.co.uk>
2022-09-27 12:16:38 +0100

    split into days

--- query generator/dategenerator.py
+++ query generator/dategenerator.py
@@ -2,11 +2,11 @@ with open("./queries.txt", 'w+') as queries:
 
     for year in range(2017, 2023):
         for month in range(1, 13):
-            next_month = month + 1
             month = str(month)
-            next_month = str(next_month)
             if len(month) == 1:
                 month = "0"+month
-            if len(next_month) == 1:
-                next_month = "0"+next_month
-            queries.write(f"https://api.gdeltproject.org/api/v2/doc/doc?query=(devops OR gitops OR kubernetes OR containerization OR \"cloud native\" OR \"machine learning\")&mode=ArtList&maxrecords=250&sort=DateAsc&format=json&STARTDATETIME={year}{month}01000000&ENDDATETIME={year}{month}31235959\n")
+                for day in range(1, 32):
+                    day = str(day)
+                    if len(day) == 1:
+                        day = "0"+day
+                        queries.write(f"https://api.gdeltproject.org/api/v2/doc/doc?query=(devops OR gitops OR kubernetes OR containerization OR \"cloud native\" OR \"machine learning\")&mode=ArtList&maxrecords=250&sort=DateAsc&format=json&STARTDATETIME={year}{month}{day}000000&ENDDATETIME={year}{month}{day}235959\n")
