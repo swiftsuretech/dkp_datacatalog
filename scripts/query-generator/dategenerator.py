b8e5a61b84459d3f99a86b918df6b15cf1280382
swiftsuretech <dave@davewhitehouse.co.uk>
2022-09-27 15:58:02 +0100

    corrected malformed url

--- scripts/query-generator/dategenerator.py
+++ scripts/query-generator/dategenerator.py
@@ -13,5 +13,5 @@ with open("./queries.txt", 'w+') as queries:
                         hour = str(hour)
                         if len(hour) == 1:
                             hour = "0"+hour
-                        queries.write(f"https://api.gdeltproject.org/api/v2/doc/doc?query=technology soureceLang:eng&mode=ArtList&maxrecords=250&sort=DateAsc&format=json&STARTDATETIME={year}{month}{day}{hour}0000&ENDDATETIME={year}{month}{day}{hour}5959\n")
+                        queries.write(f"https://api.gdeltproject.org/api/v2/doc/doc?query=technology%20soureceLang:eng&mode=ArtList&maxrecords=250&sort=DateAsc&format=json&STARTDATETIME={year}{month}{day}{hour}0000&ENDDATETIME={year}{month}{day}{hour}5959\n")
 
